a68f526dd5bf0747d7fd51a12fee749dcf9e1bd6
rocio-va <rociova94@gmail.com>
2016-07-28 15:00:03 +0200

    items

--- ShoppingCart.rb
+++ ShoppingCart.rb
@@ -3,6 +3,22 @@ require "pry"
 class ShoppingCart
 	def initialize
 		@items = []
+	end
+	def add_item (item)
+		@items.push item		
+	end
+	def checkout
+		puts "What day is today? (mon|tue|wed|thu|fri|sat|sun)"
+		day = gets.chomp
+		total_cost = 0		
+		@items.each do |item|
+			if (item.instance_of? Fruit)
+				total_cost += item.price(day)
+			else
+				total_cost += item.price
+			end
+		end		
+		total_cost
 	end	
 end
 
@@ -17,12 +33,8 @@ class Item
 end
 
 class Fruit < Item
-	def initialize (day, name, price)
-		super(name, price)
-		@day = day
-	end
-	def price
-		if (@day == "sat" && @day == "sun")
+	def price (day)
+		if (day == "sat" && day == "sun")
 			@price - @price * (10 / 100)
 		else
 			@price	
@@ -39,5 +51,20 @@ class Houseware < Item
 		end
 	end
 end	
+
+bananas = Fruit.new "Banana", 10
+orange_juice = Item.new "Orange Juice", 10
+rice = Item.new "Rice", 1
+vacuum_cleaner = Houseware.new "Vacuum Cleaner", 150
+anchovies = Item.new "Anchovies", 2
+
+rocio_cart = ShoppingCart.new
+rocio_cart.add_item(bananas)
+rocio_cart.add_item(rice)
+rocio_cart.add_item(vacuum_cleaner)
+rocio_cart.add_item(rice)
+
+print_cost = rocio_cart.checkout
+puts "the total cost of your shopping is #{print_cost}"
 
 binding.pry
