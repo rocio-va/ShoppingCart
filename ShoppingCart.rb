5cf04db6fb6534b9e8161071e75aba4149fdfa22
rocio-va <rociova94@gmail.com>
2016-08-03 09:22:08 +0200

    shoppingCart

--- ShoppingCart.rb
+++ ShoppingCart.rb
@@ -1,19 +1,18 @@
 require "pry"
 
 class ShoppingCart
-	def initialize
+	def initialize (day)
 		@items = []
+		@day = day
 	end
 	def add_item (item)
 		@items.push item		
 	end
 	def checkout
-		puts "What day is today? (mon|tue|wed|thu|fri|sat|sun)"
-		day = gets.chomp
 		total_cost = 0		
 		@items.each do |item|
 			if (item.instance_of? Fruit)
-				total_cost += item.price(day)
+				total_cost += item.price(@day)
 			else
 				total_cost += item.price
 			end
@@ -38,7 +37,7 @@ end
 class Fruit < Item
 	def price (day)
 		if (day == "sat")
-			@price - @price * (10 / 100)
+			(@price - @price * (10 / 100))
 		else
 			@price	
 		end	
@@ -48,7 +47,7 @@ end
 class Houseware < Item
 	def price
 		if (@price > 100)
-			@price - @price * (5 / 100)
+			(@price - @price * (5 / 100))
 		else
 			@price
 		end
@@ -61,7 +60,10 @@ rice = Item.new "Rice", 1
 vacuum_cleaner = Houseware.new "Vacuum Cleaner", 150
 anchovies = Item.new "Anchovies", 2
 
-rocio_cart = ShoppingCart.new
+puts "What day is today? (mon|tue|wed|thu|fri|sat|sun)"
+day = gets.chomp
+
+rocio_cart = ShoppingCart.new day
 rocio_cart.add_item(bananas)
 rocio_cart.add_item(rice)
 rocio_cart.add_item(vacuum_cleaner)
